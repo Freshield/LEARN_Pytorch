cfbb05c8f1e9dac3e59c59cc1a6621952239cb70
Freshield <zxdsw199182@gmail.com>
2019-08-24 17:29:03 +0800

    done autograd

---
+++ chp1_review/a2_autograd.py
@@ -0,0 +1,60 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@License: (C) Copyright 2018, BEIJING LINKING MEDICAL TECHNOLOGY CO., LTD.
+@Contact: yangyufresh@163.com
+@File: a2_autograd.py
+@Time: 2019-10-17 17:04
+@Last_update: 2019-10-17 17:04
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+import torch
+
+x = torch.ones(2, 2, requires_grad=True)
+print(x)
+
+y = x + 2
+print(y)
+
+print(y.grad_fn)
+
+z = y * y * 3
+out = z.mean()
+print(z, out)
+
+a = torch.randn(2, 2)
+a = ((a * 3) / (a - 1))
+b = (a * a).sum()
+print(b)
+print(a.requires_grad)
+a.requires_grad_(True)
+print(a.requires_grad)
+b = (a * a).sum()
+print(b)
+
+out.backward()
+print(x.grad)
+
+x = torch.randn(3, requires_grad=True)
+y = x * 2
+while y.data.norm() < 1000:
+    y = y * 2
+
+print(y)
+v = torch.tensor([0.1, 1.0, 0.0001], dtype=torch.float)
+y.backward(v)
+
+print(x.grad)
+
+print(x.requires_grad)
+print((x ** 2).requires_grad)
+
+with torch.no_grad():
+    print((x ** 2).requires_grad)
